d07fba4566f9842a984e5b3d0c140ddc2e5e1577
garfbit <104676660+garfbit@users.noreply.github.com>
2025-08-28 06:30:57 -0500

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,9 @@
-
+input.onButtonPressed(Button.A, function () {
+    cantidad_de_personas += 1
+    basic.showNumber(cantidad_de_personas)
+})
+input.onButtonPressed(Button.B, function () {
+    cantidad_de_personas = 0
+})
+let cantidad_de_personas = 0
+cantidad_de_personas = 0
